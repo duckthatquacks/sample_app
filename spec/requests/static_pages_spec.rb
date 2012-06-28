ccea1852393e1e29dbf1d9478ade043ab4c1a9da
John Bestoso <johnbestoso@gmail.com>
2012-06-28 13:12:50 -0400

    Added Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -46,6 +46,18 @@ describe "StaticPages" do
         end
     end
     
+   # Contact page tests
+   describe "Contact page" do
+      it "should have the title 'Contact'" do
+         visit '/static_pages/contact'
+         page.should have_selector('title',
+                     :text => "Ruby on Rails Tutorial Sample App | Contact")
+      end
     
+      it "should have the h1 'Contact'" do
+         visit '/static_pages/contact'
+         page.should have_selector('h1', :text => 'Contact')
+      end
+   end
     
 end
