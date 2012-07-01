6436870077f33fd76f1c30bb142a9ecc1d485d9f
John Bestoso <johnbestoso@gmail.com>
2012-07-01 11:04:26 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,18 @@ class User < ActiveRecord::Base
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
    
    before_save { self.email.downcase! }
+   before_save :create_remember_token
    
    validates :name, presence: true, length: { maximum: 50 }
    
    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
    
-   validates :password, presence: true, length: { minimum: 6 }
+   validates :password, length: { minimum: 6 }
    validates :password_confirmation, presence: true
+   
+   private
+      def create_remember_token
+         self.remember_token = SecureRandom.urlsafe_base64
+      end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,20 @@ describe "UserPages" do
          it "should not create a user" do
             expect { click_button submit }.not_to change(User, :count)
          end
+         
+         describe "after submission" do
+            before { click_button submit }
+            
+            it { should have_selector('title', text: 'Sign Up') }
+            it { should have_content('error') }
+            it { should have_selector('li', text: '* Password can\'t be blank') }
+            it { should have_selector('li', text: '* Name can\'t be blank') }
+            it { should have_selector('li', text: '* Email can\'t be blank') }
+            it { should have_selector('li', text: '* Email is invalid') }
+            it { should have_selector('li', text: '* Password can\'t be blank') }
+            it { should have_selector('li', text: '* Password is too short (minimum is 6 characters)') }
+            it { should have_selector('li', text: '* Password confirmation can\'t be blank') }
+         end
       end
       
       describe "with valid information" do
@@ -42,6 +56,16 @@ describe "UserPages" do
          it "should create a user" do
             expect { click_button submit }.to change(User, :count).by(1)
          end
+         
+         describe "after saving the user" do
+            before { click_button submit }
+            let(:user) { User.find_by_email('user@example.com') }
+            
+            it { should have_selector('title', text: user.name) }
+            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+            it { should have_link('Sign out') }
+         end
+         
       end
    end
 end
